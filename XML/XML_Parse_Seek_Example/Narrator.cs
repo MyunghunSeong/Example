b893ff59294bf53c8782af6850871aa13e667301
성명훈 <gns53869107@gmail.com>
2018-04-05 09:01:56 +0900

    Linq를 사용하여 XML문서의 특정한 정보를 검색하는 예제 프로그램
    Signed-off-by: 성명훈 <gns53869107@gmail.com>

---
+++ XML/XML_Parse_Seek_Example/Narrator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace seek_ex
+{
+    class Narrator
+    {
+        public Program main = new Program();
+
+        public void init_System(List<string> group, IEnumerable<string> item)
+        {
+            Console.WriteLine("==== Select Element ====");
+            Console.WriteLine("1. Group");
+            Console.WriteLine("2. Item");
+            Console.WriteLine();
+
+            Console.Write("Selection : ");
+            string sel = Console.ReadLine();
+
+            switch (Convert.ToInt32(sel))
+            {
+                case 1 :
+                    startGroup("Group", group);
+                    break;
+                case 2:
+                    startItem("item", item);
+                    break;
+            }
+        }
+
+        public void startGroup(string target, List<string> group)
+        {
+            Console.WriteLine("==== Select" + target + "====");
+            for (int i = 0; i < group.Count; i++)
+            {
+                Console.WriteLine("{0}. {1}", i + 1, group[i]); 
+            }
+            Console.WriteLine();
+
+            Console.Write("Selection : ");
+            string sel = Console.ReadLine();
+
+            main.Seek_Group(target, group[Convert.ToInt32(sel) - 1]);
+        }
+
+        public void startItem(string target, IEnumerable<string> items)
+        {
+            int count = 0;
+            List<string> item_list = new List<string>();
+
+            Console.WriteLine("==== Select" + target + "====");
+            foreach(string item in items)
+            {
+                count++;
+                Console.WriteLine("{0}. {1}", count, item);
+                item_list.Add(item);
+            }
+            Console.WriteLine();
+
+            Console.Write("Selection : ");
+            string sel = Console.ReadLine();
+
+            main.Seek_Item(target, item_list[Convert.ToInt32(sel) - 1]);
+        }
+    }
+}
---
+++ XML/XML_Parse_Seek_Example/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace seek_ex
+{
+    class Program
+    {
+        public void Seek_Group(string targetName, string target)
+        {
+            XElement root = XElement.Load("group.xml");
+            IEnumerable<XElement> group =
+                from el in root.Elements(targetName)
+                where (string)el.Attribute("Name") == target
+                select el;
+
+            foreach (XElement el in group)
+                Console.WriteLine(el);
+        }
+
+        public void Seek_Item(string targetName, string target)
+        {
+            XElement root = XElement.Load("group.xml");
+            IEnumerable<XElement> item =
+                from el in root.Descendants(targetName)
+                where (string)el.Attribute("Type") == target
+                select el;
+
+            foreach (XElement el in item)
+                Console.WriteLine(el);
+        }
+
+        static void Find_Element()
+        {
+            Narrator narrator = new Narrator();
+            XElement root = XElement.Load("group.xml");
+            List<string> GroupAttrValue = new List<string>();
+            List<string> ItemAttrValue = new List<string>();
+
+            IEnumerable<XAttribute> group =
+                from Gel in root.Elements("Group").Attributes()
+                select Gel;
+
+            IEnumerable<XAttribute> item =
+                from Iel in root.Descendants("item").Attributes()
+                select Iel;
+
+            foreach (XAttribute Iel in item)             
+                 ItemAttrValue.Add(Iel.Value);
+
+            var distin_Item = ItemAttrValue.Select(a => a).Distinct();
+
+            foreach (XAttribute Gel in group)
+                GroupAttrValue.Add(Gel.Value);
+
+            narrator.init_System(GroupAttrValue, distin_Item);
+        }
+
+        static void Main(string[] args)
+        {
+            Find_Element();
+        }
+    }
+}
